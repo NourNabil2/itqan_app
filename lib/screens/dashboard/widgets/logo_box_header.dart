314ba5026a51f6ab0406cfb1cc28484d8675437a
Nour Nabil <nour60g@gmail.com>
2025-09-22 15:44:01 +0300

    header app

---
+++ lib/screens/dashboard/widgets/logo_box_header.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:itqan_gym/core/utils/app_size.dart';
+
+class LogoBoxHeader extends StatelessWidget {
+  final String title;
+  final String? subtitle;
+  final String assetLogo; // مسار لوجو التطبيق
+
+  const LogoBoxHeader({
+    super.key,
+    required this.title,
+    this.subtitle,
+    required this.assetLogo,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      color: Theme.of(context).scaffoldBackgroundColor,
+      padding: EdgeInsets.symmetric(vertical: SizeApp.padding * 2, horizontal: SizeApp.padding),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Image.asset(
+            assetLogo,
+            width: 80.w,
+            height: 80.w,
+            fit: BoxFit.contain,
+          ),
+          SizedBox(height: SizeApp.padding),
+          Column(
+           mainAxisAlignment: MainAxisAlignment.start,
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: [
+             Text(
+               title,
+               style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                 color: const Color(0xFF2C3E50),
+                 fontWeight: FontWeight.bold
+               ),
+               textAlign: TextAlign.center,
+             ),
+             if (subtitle != null && subtitle!.isNotEmpty) ...[
+               SizedBox(height: 6.h),
+               Text(
+                 subtitle!,
+                 style: Theme.of(context).textTheme.labelMedium,
+                 textAlign: TextAlign.center,
+               ),
+             ],
+           ],
+         ),
+        ],
+      ),
+    );
+  }
+}
